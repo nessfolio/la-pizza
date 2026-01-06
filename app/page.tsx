d4b04d1c8ab7de1f0e001d998101c3b7029e1d69
ness <nessfolio@gmail.com>
2026-01-06 16:09:23 +0600

    Install ui components from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Button variant="outline">Basket</Button>
+      <Button variant={"outline"}>Корзина</Button>
+      <Skeleton>yo</Skeleton>
+      <Input></Input>
     </div>
   );
 }
